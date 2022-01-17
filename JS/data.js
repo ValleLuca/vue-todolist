8a9c8518adb33a6fa92dd93e3e992210e04e22e7
ValleLuca <valle.lucaAb@outlook.it>
2022-01-17 15:55:35 +0100

    Array con oggetti

--- JS/data.js
+++ JS/data.js
@@ -1,6 +1,30 @@
 var app = new Vue({
     el: '#containerMain',
     data: {
-      
+      texTodo: "",
+      todoList: [
+        {
+          componente: "latte",
+          fatto: false
+        },
+        {
+          componente: "carne",
+          fatto: false
+        },
+        {
+          componente: "miele",
+          fatto: false
+        },
+        {
+          componente: "verdura",
+          fatto: false
+        }
+      ]      
+    },
+    methods: {
+      addTodo: function() {
+        this.todoList.push(this.texTodo);
+        this.texTodo = "";
+      }
     }
   })
