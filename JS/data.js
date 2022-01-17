3cb577eb8345d6dd395a8d7459390240a1c8e12f
ValleLuca <valle.lucaAb@outlook.it>
2022-01-17 17:55:54 +0100

    Aggiunto inserimento in array con più oggetti

--- JS/data.js
+++ JS/data.js
@@ -2,29 +2,19 @@ var app = new Vue({
     el: '#containerMain',
     data: {
       texTodo: "",
-      todoList: [
-        {
-          componente: "latte",
-          fatto: false
-        },
-        {
-          componente: "carne",
-          fatto: false
-        },
-        {
-          componente: "miele",
-          fatto: false
-        },
-        {
-          componente: "verdura",
-          fatto: false
-        }
-      ]      
+      todoList: []   
     },
     methods: {
       addTodo: function() {
-        this.todoList.push(this.texTodo);
+        this.todoList.push({
+          componente: this.texTodo,
+          fatto: false,
+          rimuovi: false
+        });
         this.texTodo = "";
+      },
+      removeClass: function() {
+        this.rimuovi = true;
       }
     }
   })
